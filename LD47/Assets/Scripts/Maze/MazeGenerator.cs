74378b84de45f3f3089b4091ed139548700b08ce
Kylie <kr.gilde@gmail.com>
2020-10-03 12:41:11 -0400

    Maze cell
    Created a cell that can be given any size and have its walls destroyed

---
+++ LD47/Assets/Scripts/Maze/MazeGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public int sections;
+    public Vector2 dimensions;
+    public GameObject checkpoint;
+
+
+}
