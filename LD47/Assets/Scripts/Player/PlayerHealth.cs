187e829093b886e92266863cf71f2017434fb0d6
Kylie <kr.gilde@gmail.com>
2020-10-05 16:59:03 -0400

    Checkpoint lighting
    Too hard to see chekcpoints, fixed that

--- LD47/Assets/Scripts/Player/PlayerHealth.cs
+++ LD47/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : Health
 {
+    public Image damageFlash;
+    public float flashFadeSpeed;
+    public Color damageFlashColor = Color.red;
+
     public event Action OnPlayerDeath;
 
     private void OnEnable()
@@ -26,5 +31,22 @@ public class PlayerHealth : Health
         transform.rotation = Quaternion.Euler(Vector3.zero);
         currentHealth = maxHealth;
         UpdateHealthbar();
+    }
+
+    public override void TakeDamage(float dmg)
+    {
+        base.TakeDamage(dmg);
+
+        StartCoroutine(DamageFlash());
+    }
+
+    private IEnumerator DamageFlash()
+    {
+        damageFlash.color = damageFlashColor;
+        while (damageFlash.color.a >= 0)
+        {
+            damageFlash.color = Color.Lerp(damageFlash.color, Color.clear, flashFadeSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
